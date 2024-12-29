44786294a7b29bc503b763eb2edc9139562776fc
Mr Z <denver.cfman@gmail.com>
2024-12-28 20:20:07 -0700

    Update common-rpi0-node.nix

--- common-rpi0-node.nix
+++ common-rpi0-node.nix
@@ -1,1 +1,47 @@
-###
+{
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ./sd-image.nix
+    ./common-aarch64.nix
+  ];
+
+  sdImage = {
+    compressImage = false;
+    imageName = "common-rpi0.img";
+
+    extraFirmwareConfig = {
+      # Give up VRAM for more Free System Memory
+      # - Disable camera which automatically reserves 128MB VRAM
+      start_x = 0;
+
+      # Reduce allocation of VRAM to 16MB minimum for non-rotated
+      # (32MB for rotated)
+      gpu_mem = 16;
+
+      # Configure display to 800x600 so it fits on most screens
+      # * See: https://elinux.org/RPi_Configuration
+      hdmi_group = 2;
+      hdmi_mode = 8;
+    };
+  };
+
+  # this is handled by nixos-hardware on Pi 4
+  ## "console=ttyS1,115200n8" OR "console=ttyAMA0,115200n8" 
+  boot = {
+    kernelParams = lib.mkForce ["console=ttyAMA0,115200n8" "console=ttyS1,115200n8"];
+    initrd.availableKernelModules = [
+      "usbhid"
+      "usb_storage"
+    ];
+  };
+
+  networking = { 
+    wireless.enable = false;
+  };
+
+}
