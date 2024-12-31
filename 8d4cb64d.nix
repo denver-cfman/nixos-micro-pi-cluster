a27e0fd961d91573c4dc202cd68627bd33f1dab3
Mr Z <denver.cfman@gmail.com>
2024-12-30 18:56:04 -0700

    Update 8d4cb64d.nix

--- 8d4cb64d.nix
+++ 8d4cb64d.nix
@@ -85,14 +85,14 @@
     };
   };
 
-  networking.interfaces.br0.ipv4.addresses = [ {
+  networking.interfaces.eth0.ipv4.addresses = [ {
     address = "10.0.85.10";
     prefixLength = 24;
   } ];
 
   networking.defaultGateway = {
     address = "10.0.85.1";
-    interface = "br0";
+    interface = "eth0";
   };
 
   networking.nameservers = [
