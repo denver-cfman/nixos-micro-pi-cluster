c7a3cd1da8ca020c1fd746284e4b5146f9003738
Mr Z <denver.cfman@gmail.com>
2025-01-02 15:58:09 -0700

    Update 8d4cb64d.nix

--- 8d4cb64d.nix
+++ 8d4cb64d.nix
@@ -85,7 +85,6 @@
     wireless.enable = false;
   };
 
-  #networking.dhcpcd.denyInterfaces = [ "eth0" "eth1" "eth2" "eth3" "eth4" ];
   networking.useDHCP = lib.mkForce false;
 
   networking.bridges = {
@@ -112,31 +111,6 @@
     "8.8.8.8"
     "1.1.1.1"
   ];
-
-/*
-
-  services.dnsmasq = {
-    enable = true;
-    reloadIfChanged = false;
-    restartIfChanged = false;
-    resolveLocalQueries = false;
-    settings = {
-      dhcp-authoritative = true;
-      bind-interfaces = true;
-      domain-needed = true;
-      expand-hosts = true;
-      bogus-priv = true;
-      no-resolv = true;
-      no-hosts = true;
-      log-dhcp = true;
-      no-poll = true;
-      interface = [ "usb0" ];
-      dhcp-range = [ "172.16.1.2,172.16.1.253" ];
-    };
-  };
-
-*/
-
 
   systemd.services."cluster-hat" = {
     description = "MicroPi ClusterHat Prep Script";
@@ -180,8 +154,13 @@
             ];
     script = ''
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000001)) 1 0x20 1 0xff # Node 1
+      ${pkgs.coreutils}/bin/sleep 45
+      ${pkgs.bridge-utils}/bin/brctl addif br0 node1 || true
+      ${pkgs.nettools}/bin/ifconfig node1 up || true
     '';
     preStop = ''
+      ${pkgs.nettools}/bin/ifconfig node1 down
+      ${pkgs.bridge-utils}/bin/brctl delif br0 node1 || true
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000001)) 1 0x20 1 0x00 # Node 1
     '';
   };
@@ -201,8 +180,13 @@
             ];
     script = ''
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000010)) 1 0x20 1 0xff # Node 2
+      ${pkgs.coreutils}/bin/sleep 45
+      ${pkgs.bridge-utils}/bin/brctl addif br0 node2 || true
+      ${pkgs.nettools}/bin/ifconfig node2 up || true
     '';
     preStop = ''
+      ${pkgs.nettools}/bin/ifconfig node2 down
+      ${pkgs.bridge-utils}/bin/brctl delif br0 node2 || true
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000010)) 1 0x20 1 0x00 # Node 2
     '';
   };
@@ -222,8 +206,13 @@
             ];
     script = ''
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000100)) 1 0x20 1 0xff # Node 3
+      ${pkgs.coreutils}/bin/sleep 45
+      ${pkgs.bridge-utils}/bin/brctl addif br0 node3 || true
+      ${pkgs.nettools}/bin/ifconfig node3 up || true
     '';
     preStop = ''
+      ${pkgs.nettools}/bin/ifconfig node3 down
+      ${pkgs.bridge-utils}/bin/brctl delif br0 node3 || true
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00000100)) 1 0x20 1 0x00 # Node 3
     '';
   };
@@ -253,54 +242,6 @@
       ${pkgs.i2c-tools}/bin/i2cset -y -m $((2#00001000)) 1 0x20 1 0x00 # Node 4
     '';
   };
-
-/*
-
-  systemd.services."usb-otg" = {
-    enable = false;
-    reloadIfChanged = false;
-    restartIfChanged = false;
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    wantedBy = [ "default.target" ];
-    script = ''
-      ${pkgs.kmod}/bin/modprobe libcomposite
-      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/8d4cb64d
-      cd /sys/kernel/config/usb_gadget/8d4cb64d
-      echo 0x1d6b > idVendor # Linux Foundation
-      echo 0x0104 > idProduct # Multifunction Composite Gadget
-      echo 0x0100 > bcdDevice # v1.0.0
-      echo 0x0200 > bcdUSB # USB2
-      echo 0xEF > bDeviceClass
-      echo 0x02 > bDeviceSubClass
-      echo 0x01 > bDeviceProtocol
-      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/8d4cb64d/strings/0x409
-      echo "fedcba8d4cb64d" > strings/0x409/serialnumber
-      echo "TheWifiNinja" > strings/0x409/manufacturer
-      echo "PI4 USB Device" > strings/0x409/product
-      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/8d4cb64d/configs/c.1/strings/0x409
-      echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
-      echo 250 > configs/c.1/MaxPower
-      # Add functions here
-      # see gadget configurations below
-      # End functions
-      ${pkgs.coreutils}/bin/rm -fv /sys/kernel/config/usb_gadget/8d4cb64d/configs/c.1/ecm.usb0 || true
-      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/8d4cb64d/functions/ecm.usb0
-      HOST="00:dc:c8:12:7c:b3"
-      SELF="b8:27:eb:12:7c:b2"
-      echo $HOST > /sys/kernel/config/usb_gadget/8d4cb64d/functions/ecm.usb0/host_addr
-      echo $SELF > /sys/kernel/config/usb_gadget/8d4cb64d/functions/ecm.usb0/dev_addr
-      ln -s /sys/kernel/config/usb_gadget/8d4cb64d/functions/ecm.usb0 configs/c.1/
-      ${pkgs.systemd}/bin/udevadm settle -t 5 || :
-      ls /sys/class/udc > UDC
-    '';
-  };
-  #systemd.services.dnsmasq.after = [ "usb-otg.service" ];
-  #systemd.services."network-addresses-usb0".after = [ "usb-otg.service" ];
-  
-  */
 
     # Enable OpenSSH out of the box.
   services.sshd.enable = true;
