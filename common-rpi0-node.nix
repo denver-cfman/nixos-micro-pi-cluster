996fdcd2bad4041079512aaea6da74e9f011c8a2
Mr Z <denver.cfman@gmail.com>
2025-01-03 01:25:55 -0700

    Update common-rpi0-node.nix

--- common-rpi0-node.nix
+++ common-rpi0-node.nix
@@ -35,6 +35,7 @@
   boot = {
     kernelParams = lib.mkForce [
                                   "console=ttyS1,115200n8"
+                                  "console=ttyGS0,115200n8"
                               ];
     initrd.availableKernelModules = [
       "usbhid"
