3d077a1ac73c97a8c829117ce0ae2aea6ec071da
Mr Z <denver.cfman@gmail.com>
2025-01-03 15:01:41 -0700

    Update 004f17e5.nix

--- 004f17e5.nix
+++ 004f17e5.nix
@@ -19,7 +19,11 @@ in
   sdImage.imageName  = lib.mkForce "${pi-sn}.img";
   networking.hostName = lib.mkForce "${pi-sn}";
 
-  
+    boot.kernelParams = lib.mkForce [
+                                  "console=ttyS1,115200n8"
+                                  #"console=ttyGS0,115200n8"
+                              ];
+
   systemd.services."usb-otg" = {
     serviceConfig = {
       Type = "oneshot";
