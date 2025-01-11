7f4ec10050eb03909efaf883cd52586712138fb9
Mr Z <denver.cfman@gmail.com>
2025-01-11 16:47:14 -0700

    Update common-rpi0-node.nix

--- common-rpi0-node.nix
+++ common-rpi0-node.nix
@@ -6,7 +6,7 @@
   ...
 }:
 let
-  k3sToken = (builtins.getEnv "K3S_TOKEN" or "somereallylongfakevaluegoeshere"); # Replaceable variable
+  k3sToken = if builtins.hasEnv "K3S_TOKEN" then builtins.getEnv "K3S_TOKEN" else "somereallylongfakevaluegoeshere";
 in
 {
   imports = [
