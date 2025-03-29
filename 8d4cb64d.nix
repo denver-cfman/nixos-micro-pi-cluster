b9463137eb56ac667a7c0dcccc5affd6d2d7db05
Mr Z <denver.cfman@gmail.com>
2025-03-29 17:28:00 -0600

    Update 8d4cb64d.nix

--- 8d4cb64d.nix
+++ 8d4cb64d.nix
@@ -60,7 +60,7 @@
     KERNEL=="gpiomem", GROUP="gpio", MODE="0660"
     SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", PROGRAM="${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/chgrp -R gpio /sys/class/gpio && ${pkgs.coreutils}/bin/chmod -R g=u /sys/class/gpio'"
     SUBSYSTEM=="gpio", ACTION=="add", PROGRAM="${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/chgrp -R gpio /sys%p && ${pkgs.coreutils}/bin/chmod -R g=u /sys%p'"
-    ACTION=="add", SUBSYSTEMS=="usb", DRIVERS=="usb", ATTRS{manufacturer}=="GiezenConsulting", ATTRS{serial}=="1b5a4d6b", NAME="node1"
+    #ACTION=="add", SUBSYSTEMS=="usb", DRIVERS=="usb", ATTRS{manufacturer}=="GiezenConsulting", ATTRS{serial}=="1b5a4d6b", NAME="node1"
     ACTION=="add", SUBSYSTEMS=="usb", DRIVERS=="usb", ATTRS{manufacturer}=="GiezenConsulting", ATTRS{serial}=="1b5a4d6b", NAME="node1", PROGRAM="${pkgs.bash}/bin/bash -c '${pkgs.nettools}/bin/ifconfig node1 up || true && ${pkgs.bridge-utils}/bin/brctl addif br0 node1 || true'"
     ACTION=="remove", SUBSYSTEMS=="usb", DRIVERS=="usb", ATTRS{manufacturer}=="GiezenConsulting", ATTRS{serial}=="1b5a4d6b", NAME="node1", PROGRAM="${pkgs.bash}/bin/bash -c '${pkgs.nettools}/bin/ifconfig node1 down || true && ${pkgs.bridge-utils}/bin/brctl delif br0 node1 || true'"
     ACTION=="add", SUBSYSTEMS=="usb", DRIVERS=="usb", ATTRS{manufacturer}=="GiezenConsulting", ATTRS{serial}=="fe127cb3", NAME="node2"
